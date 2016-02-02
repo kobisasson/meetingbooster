8dd280d67e3b00cc930aed39e2647ab17cb5d062
Kobi Sasson <kobi.sasson@sap.com>
2016-02-03 00:51:01 +0200

    beutify code

--- test/Calendar.spec.js
+++ test/Calendar.spec.js
@@ -11,22 +11,13 @@ Promise.promisifyAll(shell);
 describe('Calendar.spec test', function () {
 
     describe('Test basic APIs', function () {
-        var icsEvent = null, eventSettings = null;
-
-        before(function () {
-
-            //shell.rm("./test/CalendarTest/Boris-Guzman.ics");
-        })
-        afterEach(function () {
-
-        })
-        it('Calendar command line test', function (done) {
+       it('Calendar command line test', function (done) {
             shell.cd( './test/CalendarTest/');
             shell.execAsync('node app.js')
                 .then(function () {
                     shell.cd(dirname);
-                    var expected = fs.readFileAsync( "./test/CalendarTest/expected/Boris-Guzman.ics", 'utf-8');
-                    var actual = fs.readFileAsync(  "./test/CalendarTest/Boris-Guzman.ics", 'utf-8');
+                    const expected = fs.readFileAsync( "./test/CalendarTest/expected/Boris-Guzman.ics", 'utf-8');
+                    const actual = fs.readFileAsync(  "./test/CalendarTest/Boris-Guzman.ics", 'utf-8');
                     Promise.props({
                         expected: expected,
                         actual: actual
@@ -34,7 +25,6 @@ describe('Calendar.spec test', function () {
                         assert.equal(result.expected, result.actual);
                         done();
                     }).done(null,function (error) {
-                        //console.log(error);
                         done(error);
                     });
                 }).done (null, function (error) {
